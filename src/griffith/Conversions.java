90c8ec7ec1807f89f90998b4c0563e6a06e40800
Anaissuh13 <hussainah2407@gmail.com>
2025-03-03 15:35:14 +0000

    Added the method stubs in the Conversions class.

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,5 +1,28 @@
 package griffith;
 
 public class Conversions {
+	// Converts Euro to Dollar
+    public double euroToDollar(double euro) {
+        return 0; // Stub
+    }
 
+    // Converts Dollar to Euro
+    public double dollarToEuro(double dollar) {
+        return 0; // Stub
+    }
+
+    // Converts String to Integer
+    public int stringToInteger(String val) {
+        return 0; // Stub
+    }
+
+    // Converts Integer to String
+    public String integerToString(int val) {
+        return null; // Stub
+    }
+
+    // Switches uppercase to lowercase and vice versa
+    public String switchCase(String input) {
+        return null; // Stub
+    }
 }
